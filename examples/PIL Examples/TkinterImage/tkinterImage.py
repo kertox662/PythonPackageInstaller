80489f328a9e2d5961a228b4aaae4b650da20f52
Misha Melnyk <misha@melnyk.org>
2020-01-08 19:57:03 -0500

    Added MouseFollower example

--- examples/PIL Examples/TkinterImage/tkinterImage.py
+++ examples/PIL Examples/TkinterImage/tkinterImage.py
@@ -6,9 +6,11 @@ canv = Canvas(root, width = 800, height = 600, background = "gray")
 canv.pack()
 
 img = Image.open("testImage.png")
-imgTk = ImageTk.PhotoImage(img)
+imgTk = ImageTk.PhotoImage(img) #Converts the image into something that can be displayed in tkinter
 
-canv.create_image(imgTk.width()/2, imgTk.height()/2, image = imgTk)
+
+#The image is displayed based on the image center.
+canv.create_image(imgTk.width()/2, imgTk.height()/2, image = imgTk) 
 canv.update()
 
 root.mainloop()
